008257b4c2c194e08ff35cd1a4268f51e01bdbd3
Amadeusz Winogrodzki <amadeusz.winogrodzki@rst.com.pl>
2018-03-09 23:52:06 +0100

    fix: fixed tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
-export { default as get } from 'lodash-es/get';
-export { default as set } from 'lodash-es/set';
-export { default as eq } from 'lodash-es/eq';
-export { default as mergeWith } from 'lodash-es/mergeWith';
-export { default as isArray } from 'lodash-es/isArray';
-export { default as isObject } from 'lodash-es/isObject';
+export {
+  get,
+  set,
+  eq,
+  mergeWith,
+  isArray,
+  isObject,
+} from 'lodash';
